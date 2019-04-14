9400233caa37168878eab017835a28e954411249
Javan R <mail@javan.de>
2019-04-14 20:29:47 +0200

    fixed bug when no internet connection

--- update.sh
+++ update.sh
@@ -8,8 +8,8 @@
 
 # check if sudo is used
 if [ "$(id -u)" != 0 ]; then
-  echo 'Sorry, you need to run this script with sudo'
-  exit 1
+    echo 'Sorry, you need to run this script with sudo'
+    exit 1
 fi
 
 # target directory
@@ -17,12 +17,9 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # install HoneyPi rpi-scripts
 echo '>>> Install HoneyPi runtime measurement scripts'
-rm -rf rpi-scripts # remove folder to download latest
-if [ -d "rpi-scripts" ]; then
-  echo 'Seems HoneyPi rpi-scripts is installed already, skip this step.'
-else
-  TAG=$(curl --silent "https://api.github.com/repos/Honey-Pi/rpi-scripts/releases/latest" | grep -Po '"tag_name": "\K.*?(?=")')
-  if [ $TAG ]; then
+TAG=$(curl --silent "https://api.github.com/repos/Honey-Pi/rpi-scripts/releases/latest" | grep -Po '"tag_name": "\K.*?(?=")')
+if [ $TAG ]; then
+    rm -rf rpi-scripts # remove folder to download latest
     echo ">>> Downloading rpi-scripts $TAG"
     wget "https://codeload.github.com/Honey-Pi/rpi-scripts/zip/$TAG" -O HoneyPiScripts.zip
     unzip HoneyPiScripts.zip
@@ -33,19 +30,16 @@ else
     echo '>>> Set file rights to python scripts'
     chown -R pi:pi $DIR/rpi-scripts
     chmod -R 775 $DIR/rpi-scripts
-  else
+else
     echo '>>> Something went wrong. Updating rpi-scripts skiped.'
-  fi
 fi
+
 
 # install HoneyPi rpi-webinterface
 echo '>>> Install HoneyPi webinterface'
-rm -rf /var/www/html # remove folder to download latest
-if [ -d "/var/www/html" ]; then
-  echo 'Seems HoneyPi rpi-webinterface is installed already, skip this step.'
-else
-  TAG=$(curl --silent "https://api.github.com/repos/Honey-Pi/rpi-webinterface/releases/latest" | grep -Po '"tag_name": "\K.*?(?=")')
-  if [ $TAG ]; then
+TAG=$(curl --silent "https://api.github.com/repos/Honey-Pi/rpi-webinterface/releases/latest" | grep -Po '"tag_name": "\K.*?(?=")')
+if [ $TAG ]; then
+    rm -rf /var/www/html # remove folder to download latest
     echo ">>> Downloading rpi-webinterface $TAG"
     wget "https://codeload.github.com/Honey-Pi/rpi-webinterface/zip/$TAG" -O HoneyPiWebinterface.zip
     unzip HoneyPiWebinterface.zip
@@ -58,7 +52,6 @@ else
     echo '>>> Set file rights to webinterface'
     chown -R www-data:www-data /var/www/html
     chmod -R 775 /var/www/html
-  else
+else
     echo '>>> Something went wrong. Updating rpi-webinterface skiped.'
-  fi
 fi
