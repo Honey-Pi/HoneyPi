b687fb8fd4791698b75937f7c097370e91b4ed36
elschnorro77 <57951003+elschnorro77@users.noreply.github.com>
2020-01-16 13:27:55 +0100

    Update and rename create_uap to create_uap.sh

--- create_uap.sh
+++ create_uap.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # Create the virtual device
 /sbin/iw dev wlan0 interface add uap0 type __ap
+ip link set uap0 up
+ip addr add 192.168.4.1/24 broadcast 192.168.4.255 dev uap0
 ifup uap0
 # Fetch wifi channel
 CHANNEL=`iwlist wlan0 channel | grep Current | sed 's/.*Channel \([0-9]*\).*/\1/g'`
