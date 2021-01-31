90c0968601971d628eb898643a8100e99f59d5ef
Javan Rasokat <mail@javan.de>
2021-01-31 21:00:05 +0100

    fixed

--- update.sh
+++ update.sh
@@ -20,7 +20,7 @@ STABLE=${1:-1} # default: 1 (stable)
 
 # Update CA certs for a secure connection to GitHub
 echo '>>> Update CA certs for a secure connection to GitHub'
-update-ca-certificates -f
+update-ca-certificates
 
 function get_latest_release() {
     REPO=$1
@@ -40,8 +40,8 @@ ScriptsTag=$(get_latest_release $REPO $STABLE)
 echo ">>> Install latest HoneyPi runtime measurement scripts ($ScriptsTag) from $REPO stable=$STABLE"
 if [ ! -z "$ScriptsTag" ]; then
     [ -f $DIR/rpi-scripts/error.log ] && mv $DIR/rpi-scripts/error.log $DIR/error.log.backup
-    CURRENTDATETIME='date +"%Y-%m-%d_%T"'
-    mv $DIR/rpi-scripts $DIR/rpi-scripts.$CURRENTDATETIME
+    CURRENTDATETIME=`date +%Y-%m-%d_%T`
+    mv $DIR/rpi-scripts "$DIR/rpi-scripts-$CURRENTDATETIME"
     rm -rf $DIR/rpi-scripts # remove folder to download latest
     echo ">>> Downloading latest rpi-scripts ($ScriptsTag)"
     wget -q "https://codeload.github.com/$REPO/zip/$ScriptsTag" -O $DIR/HoneyPiScripts.zip
